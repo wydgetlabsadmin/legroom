62101ebb1c0e2a011c4d424f7d695ee74a6a69f9
dbugger <dbugger@gmail.com>
2017-07-17 19:05:36 -0700

    Make extension resilient to class name changes.

--- extension/inject_script.js
+++ extension/inject_script.js
@@ -1,6 +1,22 @@
 console.log('Enhancing Google Flights search with amenities extension.');
 
 (function() {
+  // Class prefix. Changes per GWT deployment, so need to figure out
+  // from #root.
+  var prefix = '';
+
+  /**
+   * Adds classname prefix to given string or array of strings.
+   */
+  var pfx = function(s) {
+    if (s.map) {
+      return s.map(function(a) {
+        return '.' + prefix + a;
+      });
+    }
+    return '.' + prefix + s;
+  }
+
   /**
    * @param {string} jsonStr Data from backend.
    * @return {Map<number, string>} Itinerary number to legroom
@@ -90,7 +106,7 @@ console.log('Enhancing Google Flights search with amenities extension.');
       }
       elem.appendChild(span);
     });
-    var a = n.querySelector('.OMOBOQD-d-X>.OMOBOQD-d-Sb'); // Child link.
+    var a = n.querySelector(pfx('-d-X') + '>' + pfx('-d-Sb')); // Child link.
     a.after(elem);
     return true;
   };
@@ -125,8 +141,7 @@ console.log('Enhancing Google Flights search with amenities extension.');
   };
 
   var addNotice = function(header) {
-    var header =
-        document.querySelector('.OMOBOQD-d-cc.OMOBOQD-d-S');
+    var header = document.querySelector(pfx('-d-cc') + pfx('-d-S'));
     var notice = document.createElement('a');
     notice.href = 'https://chrome.google.com/webstore/detail/legroom-please/nhonfddkgankhjilponlbdccpabaaknp';
     notice.target = 'extension';
@@ -136,7 +151,25 @@ console.log('Enhancing Google Flights search with amenities extension.');
     header.appendChild(notice);
   };
 
+  var observeForClassPrefix = function(callback) {
+    var root = document.querySelector('#root');
+    new MutationObserver(function(mutations, o) {
+      mutations.forEach(function(m) {
+        if (m.type == 'attributes' && m.attributeName == 'class') {
+          var c = findMatchingClass(m.target, /.+-f-w/);
+          callback(c.match(/[A-Z]+/)[0]);
+          o.disconnect(); // No need to observe anymore.
+        }
+      });
+    }).observe(root, { attributes: true });
+  };
+
   window.addEventListener('load', function() {
+    observeForClassPrefix(function(p) {
+      prefix = p;
+      injectStyles();
+    });
+      
     new MutationObserver(function(mutations, o) {
       mutations.forEach(function(m) {
         if (m.type != 'childList' || m.addedNodes.length <= 0) {
@@ -145,11 +178,11 @@ console.log('Enhancing Google Flights search with amenities extension.');
         for (var i = 0; i < m.addedNodes.length; i++) {
           var n = m.addedNodes.item(i);
           // -d-cc is table header.
-          if (!n.classList || !n.classList.contains('OMOBOQD-d-Lb') ||
-              n.classList.contains('OMOBOQD-d-X')) {
+          if (!n.classList || !n.classList.contains(prefix + '-d-Lb') ||
+              n.classList.contains(prefix + '-d-X')) {
             continue;
           }
-          if (n.classList.contains('OMOBOQD-d-cc')) {
+          if (n.classList.contains(prefix + '-d-cc')) {
             addNotice(n);
           } else {
             updateNode(n);
@@ -158,6 +191,44 @@ console.log('Enhancing Google Flights search with amenities extension.');
 
       });
     }).observe(document.body, { childList: true, subtree: true });
+
   });
+ 
+  // Utility functions.
+  var findMatchingClass = function(elem, regex) {
+    if (!elem.classList) {
+      return null;
+    }
+    for (var i = 0, list = elem.classList; i < list.length; i++) {
+      var c = list.item(i);
+      if (c.match(regex)) {
+        return c;
+      }
+    }
+    return null;
+  };
+ 
+  var injectStyles = function() {
+    var styleElem = document.createElement('style');
+    document.head.appendChild(styleElem);
+
+    var ss = styleElem.sheet;
+
+    var rules = [
+        'table.pfx-f-i {width: 940px}', // Layout table.
+        'div.pfx-f-e {width: 750px}', // Middle column.
+        /* Columns. */ 
+        '.pfx-d-Lb>.pfx-d-X>div.pfx-d-Bb { width: 15.2%; }',
+        '.pfx-d-Lb>.pfx-d-X>div.pfx-d-ac { width: 36.2%; }',
+        '.pfx-d-Lb>.pfx-d-X>div.pfx-d-Jb { width: 15.5%; }',
+        '.pfx-d-Lb>.pfx-d-X>div.pfx-d-Sb { width: 17.1%; }',
+    ];
+
+    rules.forEach(function(rule) {
+      rule = rule.replace(/pfx/g, prefix);
+      ss.insertRule(rule, ss.cssRules.length);
+    });
+  };
+
 })();
 
